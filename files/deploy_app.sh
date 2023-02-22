569a5f4440027fd34908f0e1a142fcfe0d5e12ed
BadPiggy <56523072+leonwli@users.noreply.github.com>
2023-02-22 16:31:55 +1100

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  The world is yours, but it is ultimately mine!
   <!-- END -->
 
   </div>
